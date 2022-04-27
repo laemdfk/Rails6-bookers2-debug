dee34ecd02aac5939deeacf42b507883d82e7dd4
EC2 Default User <ec2-user@ip-172-31-2-155.ap-northeast-1.compute.internal>
2022-04-27 12:34:53 +0000

    [Add]ルーテイング変更、syntaxエラー解消

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,13 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-
-  root :to =>"homes#top"
+ 
+  root to: "homes#top"
+  # root :toを　root to:へ変更
+  devise_for :users
   get "home/about"=>"homes#about"
 
   resources :books, only: [:index,:show,:edit,:create,:destroy,:update]
   resources :users, only: [:index,:show,:edit,:update]
-  devise_for :users
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-
+ 
+ 
+# endを追加し、ルーティングの順番変更
+end
