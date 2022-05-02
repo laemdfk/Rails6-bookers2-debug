76c278d0a7e5b51ffcb9a65649cad2f9d3470448
EC2 Default User <ec2-user@ip-172-31-2-155.ap-northeast-1.compute.internal>
2022-05-02 05:18:48 +0000

    [Add]favorites controller アクション内容追加

--- app/controllers/favorite_controller.rb
+++ app/controllers/favorite_controller.rb
@@ -1,7 +1,16 @@
 class FavoriteController < ApplicationController
   def create
+    @book_favorite = Favorite.new(user_id: current_user.id, book_id: params[:book_id])
+
+    @book_favorite.save?
+     redirect_to request.referer :back
+     #request.referer=特定の処理後、遷移元のURLに戻る（リダイレクトする）ための処理。
   end
 
   def destroy
+    @book_favorite = Favorite.new(user_id: current_user.id, book_id: params[:book_id])
+
+    @book_favorite.destroy
+     redirect_to request.referer :back
   end
 end
