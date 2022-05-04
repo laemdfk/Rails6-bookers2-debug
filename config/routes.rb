ea8a57f8b14556a2c261b231e9eeac8449451884
laemdfk <kappa021618@icloud.com>
2022-05-04 05:53:46 +0000

    [Add]ルーティング記述追加

--- config/routes.rb
+++ config/routes.rb
@@ -7,13 +7,18 @@ Rails.application.routes.draw do
   devise_for :users
   get "home/about"=>"homes#about"
 
-  resources :books do
-   resource :book_comment, only: [:create, :destroy]
+  resources :books, only: [:index,:show,:edit,:create,:destroy,:update] do
+   resources :book_comment, only: [:create, :destroy]
    resource :favorites, only: [:create, :destroy]
  end
 
 
-  resources :users, only: [:index,:show,:edit,:update]
+  resources :users, only: [:index,:show,:edit,:update]do
+   member do
+    get :follows, :followers
+   end
+   resource :relationship,only:[:create, :destroy]
+ end
 
 
 # endを追加し、ルーティングの順番変更
