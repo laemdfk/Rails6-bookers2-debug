2479b1807604ec39ffa809e44a3811ec135ec995
laemdfk <kappa021618@icloud.com>
2022-05-09 07:52:02 +0000

    [Add]favorites_controller 記述変更

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,7 +1,7 @@
 class FavoritesController < ApplicationController
   def create
-   book = Book.find(params[:book_id])
-   favorites = current_user.favorites.new(book_id: book.id)
+   @book = Book.find(params[:book_id])
+   favorites = current_user.favorites.new(book_id: @book.id)
    favorites.save
   # redirect_to request.referer
    #request.referer=特定の処理後、遷移元のURLに戻る（リダイレクトする）ための処理。
@@ -13,8 +13,8 @@ class FavoritesController < ApplicationController
   end
 
   def destroy
-    book = Book.find(params[:book_id])
-    favorites = current_user.favorites.find_by(book_id: book.id)
+    @book = Book.find(params[:book_id])
+    favorites = current_user.favorites.find_by(book_id: @book.id)
     favorites.destroy
     # redirect_to request.referer
     # @book_favorite = Favorite.find_by(user_id: current_user.id, book_id: params[:book_id])
